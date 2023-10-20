29057d1546d67210c15e441f3d99ea19badced16
LGC363 <2220801450@qq.com>
2023-10-20 17:28:03 +0800

    updata

--- Source/ActionRoguelike/SCharacter.cpp
+++ Source/ActionRoguelike/SCharacter.cpp
@@ -91,6 +91,7 @@ void ASCharacter::PrimaryAttack_TimeElapsed()
 
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	SpawnParams.Instigator = this;
 
 	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
 }
